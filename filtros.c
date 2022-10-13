05527c6a974afb068a81c3e82cbf533ffc0a15fc
JaynaPaola <a01642269@tec.mx>
2022-10-13 12:38:31 +0000

    practica 3

--- filtros.c
+++ filtros.c
@@ -3,22 +3,17 @@
 #include <ctype.h>
 
 char entrada;
-char mayuscula;
+//char mayuscula;
 int n;
 
 int main(){
     do{
         char vocal = '1';
-       // n = read(STDIN_FILENO,&c,1); //leer la entrada estándar, guardar en c y leer uno por una
-        //may = toupper(c); //Convertir a mayúsculas
-        //write(STDOUT_FILENO,&may,1);
-
-        n = read(STDIN_FILENO,&centrada,1);
-        mayuscula = toupper(entrada); 
-        if (mayuscula == 'A' || mayuscula =='E' || mayuscula =='I' || mayuscula =='O' || mayuscula =='U' ){
+        n = read(STDIN_FILENO,&entrada,1);//leer la entrada estándar, guardar en entrada y leer uno por uno
+        //mayuscula = toupper(entrada);//Convertir a mayúsculas
+        if (entrada == 'A' || entrada =='E' || entrada =='I' || entrada =='O' || entrada =='U'|| entrada =='a'|| entrada =='e'|| entrada =='i'|| entrada =='o' || entrada =='u'){
         write(STDOUT_FILENO,&vocal,1);
     }
-
     }
     while(n!=0);
     return 0;
